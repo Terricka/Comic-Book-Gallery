a136ca9524ed50890041332dac963a13005102a4
Terricka <johnt626@newschool.edu>
2017-09-20 09:25:53 -0400

    Updated Default route

--- Comic-Book-Gallery/App_Start/RouteConfig.cs
+++ Comic-Book-Gallery/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ComicBookGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBook", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
